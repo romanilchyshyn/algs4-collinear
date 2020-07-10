86fcea0e289b6ee47c8ee7e60071453646888b13
Roman Ilchyshyn <romanilchyshyn@gmail.com>
2020-07-10 23:12:56 +0300

    Fix BruteCollinearPoints corner cases.

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -24,22 +24,28 @@ public class BruteCollinearPoints {
         }
 
         for (int i = 0; i < points.length; i++) {
+            Point p = points[i];
+            BruteCollinearPoints.checkPoint(p);
+
             for (int j = i + 1; j < points.length; j++) {
+                Point q = points[j];
+                BruteCollinearPoints.checkPoint(q);
+
                 for (int m = j + 1; m < points.length; m++) {
+                    Point r = points[m];
+                    BruteCollinearPoints.checkPoint(r);
+
                     for (int n = m + 1; n < points.length; n++) {
-                        Point p = points[i];
-                        Point q = points[j];
-                        Point r = points[m];
                         Point s = points[n];
+                        BruteCollinearPoints.checkPoint(s);
 
-                        if (p == null || q == null || r == null || s == null) {
-                            throw new IllegalArgumentException();
-                        }
+                        Point[] pqrs = {p, q, r, s};
+                        Arrays.sort(pqrs);
 
-                        if (p.compareTo(q) == 0) {
-                            System.out.println(p);
-                            System.out.println(q);
-                            throw new IllegalArgumentException();
+                        for (int k = 1; k < pqrs.length; k++) {
+                            if (pqrs[k-1].compareTo(pqrs[k]) == 0) {
+                                throw new IllegalArgumentException();
+                            }
                         }
 
                         double pqSlope = p.slopeTo(q);
@@ -47,14 +53,17 @@ public class BruteCollinearPoints {
                         double psSlope = p.slopeTo(s);
 
                         if (pqSlope == prSlope && prSlope == psSlope) {
-                            Point[] pqrs = {p, q, r, s};
-                            Arrays.sort(pqrs);
-
                             segments.add(new LineSegment(pqrs[0], pqrs[3]));
                         }
                     }
                 }
             }
+        }
+    }
+
+    private static void checkPoint(Point p) {
+        if (p == null) {
+            throw new IllegalArgumentException();
         }
     }
 
